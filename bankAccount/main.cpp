9770a60b2e669b48113418bb6291b4aeaf49edb9
zuber Ab <zuber1077@yahoo.com>
2018-03-21 15:03:27 +0800

    Initial Commit

---
+++ bankAccount/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bankAccount
+//
+//  Created by Zuber on 21/03/2018.
+//  Copyright © 2018 Zuber. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
